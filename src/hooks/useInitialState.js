c47ce0998a4cf649b1278ee4c99cc22bb1338a70
cituk <ecituk@thefives.com.mx>
2022-02-23 21:52:03 -0500

    Creando nuestro primer custom hook

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import initialState from '../initialState'
+
+const useInitiaState = () => {
+    const [ state, setState ] = useState( initialState )
+
+    const addToCart = payload => {
+        setState({
+            ...state,
+            cart: [...state.cart, payload ]
+        })
+    }
+
+    const removeFromCart = payload => {
+        setState({
+            ...state,
+            cart: state.cart.filter(items => items.id !== payload.id )
+        })
+    }
+
+    return {
+        addToCart, //retorno los metodos
+        removeFromCart,
+        state //y el estado
+    }
+}
+
+export default useInitiaState
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import * as Page from '../containers/index'
 import Layout from '../components/Layout'
+import AppContext from '../context/AppContex'
+import useInitialState from '../hooks/useInitialState'
+
 
 export const App = () => {
+  const initialState = useInitialState()
   return (
-    <BrowserRouter>
-        <Layout>
-            <Routes>
-                <Route path='/' element={<Page.Home />} />
-                <Route path='/checkout' element={<Page.Checkout />} />
-                <Route path='/checkout/information' element={<Page.Information />} />
-                <Route path='/checkout/payment' element={<Page.Payment />} />
-                <Route path='/checkout/success' element={<Page.Success />} />
-                <Route path='*' element={<Page.NotFound />} />
-            </Routes>
-        </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+          <Layout>
+              <Routes>
+                  <Route path='/' element={<Page.Home />} />
+                  <Route path='/checkout' element={<Page.Checkout />} />
+                  <Route path='/checkout/information' element={<Page.Information />} />
+                  <Route path='/checkout/payment' element={<Page.Payment />} />
+                  <Route path='/checkout/success' element={<Page.Success />} />
+                  <Route path='*' element={<Page.NotFound />} />
+              </Routes>
+          </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
 
